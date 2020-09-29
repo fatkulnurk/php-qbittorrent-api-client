6dbf7c9b8d163cf8877ddeea137368d487a7769c
fatkulnurk <fatkulnurk@gmail.com>
2020-09-29 12:53:07 +0700

    add collection

---
+++ src/Collection.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace Fatkulnurk\PHPQbittorrentApi;
+
+
+use Psr\Http\Message\ResponseInterface;
+
+class Collection implements CollectionInterface
+{
+    private $response;
+
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+    }
+
+    public static function make(ResponseInterface $response)
+    {
+        return (new self($response));
+    }
+
+    public function getResponse(): ResponseInterface
+    {
+        return $this->response;
+    }
+
+    public function getStatusCode()
+    {
+        return $this->response->getStatusCode();
+    }
+
+    public function getBody()
+    {
+        return $this->response->getBody();
+    }
+
+    public function getContents()
+    {
+        return $this->response->getBody()->getContents();
+    }
+
+    public function getHeaders()
+    {
+        return $this->response->getHeaders();
+    }
+
+    public function get(): array
+    {
+        return [
+            'http_status_code' => $this->getStatusCode(),
+            'body' => $this->getBody(),
+            'contents' => $this->getContents()
+        ];
+    }
+}
---
+++ src/CollectionInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace FatkulNurK\PHPQbittorrentApi;
+
+
+use Psr\Http\Message\ResponseInterface;
+
+interface CollectionInterface
+{
+    public function getStatusCode();
+    public function getBody();
+    public function getContents();
+    public function getHeaders();
+
+    public function getResponse(): ResponseInterface;
+    public function get(): array;
+}
