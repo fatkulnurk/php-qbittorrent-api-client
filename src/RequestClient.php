28c4f1984e00a9a4679a55cfc0c66fb414844df7
fatkulnurk <fatkulnurk@gmail.com>
2020-09-29 12:50:56 +0700

    fix cs

--- src/RequestClient.php
+++ src/RequestClient.php
@@ -9,12 +9,14 @@ class RequestClient
 {
     public static function make(): ClientInterface
     {
-        $client = new Client([
+        $client = new Client(
+            [
             // Base URI is used with relative requests
             'base_uri' => Singleton::getInstance()->getBaseUri(),
             // You can set any number of default request options.
             'timeout'  => 2.0,
-        ]);
+            ]
+        );
 
         return $client;
     }
